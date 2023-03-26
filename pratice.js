131ca76f84cf71949505e3cc12a829f4f011b772
adiraushan <adiraushan2k@gmail.com>
2023-03-27 03:26:34 +0530

    fix

--- pratice.js
+++ pratice.js
@@ -36,97 +36,83 @@ These are all the DIV ID's you're gonna need access to 👇
 
 // Call the main functions the page is loaded
 window.onload = () => {
-    getOriginals()
-    getTrendingNow()
-    getTopRated()
-  }
-  
-  // ** Helper function that makes dynamic API calls **
-  function fetchMovies(url, dom_element, path_type) {
-    // Use Fetch with the url passed down 
-  fetch(url)
-  .then(response => {
-    if(response.ok){
-        return response.json()
-    } else{
-        throw new console.error('404');
-    }
-  }).then(data => { console.log(data)
-    
-  }).catch(error => {
-    console.log('404')
-  })
-    // Within Fetch get the response and call showMovies() with the data , dom_element, and path type
-  }
+  getOriginals()
+  getTrendingNow()
+  getTopRated()
+}
 
-  fetchMovies('https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213')
+// ** Helper function that makes dynamic API calls **
+function fetchMovies(url, dom_element, path_type) {
+  // Use Fetch with the url passed down 
+
+  // Within Fetch get the response and call showMovies() with the data , dom_element, and path type
+}
+
+//  ** Function that displays the movies to the DOM **
+showMovies = (movies, dom_element, path_type) => {
   
-  //  ** Function that displays the movies to the DOM **
-  showMovies = (movies, dom_element, path_type) => {
-    
-    // Create a variable that grabs id or class
-    let moviesel = document.querySelector('getTrendingNow')
+  // Create a variable that grabs id or class
+
+
+  // Loop through object
+
+
+    // Within loop create an img element
+
+
+    // Set attribute
+
+
+    // Set source
+
+
+    // Add event listener to handleMovieSelection() onClick
+
   
-    // Loop through object
-  
-  
-      // Within loop create an img element
-  
-  
-      // Set attribute
-  
-  
-      // Set source
-  
-  
-      // Add event listener to handleMovieSelection() onClick
-  
-    
-      // Append the imageElement to the dom_element selected
-  
-    }
-  
-  
-  // ** Function that fetches Netflix Originals **
-  function getOriginals() {
-  
+    // Append the imageElement to the dom_element selected
+
   }
-  // ** Function that fetches Trending Movies **
-  function getTrendingNow() {
-  
+}
+
+// ** Function that fetches Netflix Originals **
+function getOriginals() {
+
+}
+// ** Function that fetches Trending Movies **
+function getTrendingNow() {
+
+}
+// ** Function that fetches Top Rated Movies **
+function getTopRated() {
+
+}
+
+// ** BONUS **
+
+// ** Fetches URL provided and returns response.json()
+async function getMovieTrailer(id) {
+  //URL: `https://api.themoviedb.org/3/movie/${id}/videos?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US`
+
+}
+
+// ** Function that adds movie data to the DOM
+const setTrailer = trailers => {
+  // Set up iframe variable to hold id of the movieTrailer Element
+  const iframe
+  // Set up variable to select .movieNotFound element
+  const movieNotFound
+
+  // If there is a trailer add the src for it
+  if (trailers.length > 0) {
+    // add d-none class to movieNotFound and remove it from iframe
+
+    // add youtube link with trailers key to iframe.src
+  } else {
+    // Else remove d-none class to movieNotfound and ADD it to iframe
+
   }
-  // ** Function that fetches Top Rated Movies **
-  function getTopRated() {
-  
-  }
-  
-  // ** BONUS **
-  
-  // ** Fetches URL provided and returns response.json()
-  async function getMovieTrailer(id) {
-    //URL: `https://api.themoviedb.org/3/movie/${id}/videos?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US`
-  
-  }
-  
-  // ** Function that adds movie data to the DOM
-  const setTrailer = trailers => {
-    // Set up iframe variable to hold id of the movieTrailer Element
-    const iframe = ''
-    // Set up variable to select .movieNotFound element
-    const movieNotFound = ''
-  
-    // If there is a trailer add the src for it
-    if (trailers.length > 0) {
-      // add d-none class to movieNotFound and remove it from iframe
-  
-      // add youtube link with trailers key to iframe.src
-    } else {
-      // Else remove d-none class to movieNotfound and ADD it to iframe
-  
-    }
-  }
-  
-  
-  
-  
-  
+}
+
+
+
+
